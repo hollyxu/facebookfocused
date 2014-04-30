905eece1f553206bebd37dcbe43ad8fed2f2acaa
Holly Xu <hjxu@uwaterloo.ca>
2014-04-30 18:04:52 -0400

    Scrappy but working version

---
+++ contentscript.js
@@ -0,0 +1,24 @@
+// Redirect all other Facebook pages to /messages
+var redirectURL = 'https://www.facebook.com/messages';
+var needToMatch = /messages/;
+
+var curURL = window.location.href;
+if (curURL) {
+    if (!curURL.match(needToMatch)) {
+        // Sends to listener in redirect.js
+        chrome.extension.sendRequest({redirect: redirectURL});
+    }
+}
+
+// Hide annoying elements (ID)... like the Facebook navigation bar
+var divsToHide = ['pageHead', 'pagelet_ticker'];
+
+divsToHide.forEach(function(elem, index, arr) {
+    var divToHide = elem;
+    var distractingDiv = document.getElementById(divToHide);
+    if (distractingDiv) {
+        distractingDiv.style.display = 'none';
+    }
+});
+
+
