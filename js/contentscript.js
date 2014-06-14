4c829b5e60d40d6522f15df74fcff7919792aa62
Holly Xu <hjxu@uwaterloo.ca>
2014-06-14 16:02:08 -0400

    Fixes issue #5. Now, will always redirect back to messages.

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,28 +1,45 @@
-// Redirect all other Facebook pages to /messages
-var redirectURL = 'https://www.facebook.com/messages';
-var needToMatch = /messages/;
+// -------------------------------------------------- 
+// Helper Functions
 
-var curURL = window.location.href;
-if (curURL) {
-    if (!curURL.match(needToMatch)) {
-        // Sends to listener in redirect.js
-        chrome.extension.sendRequest({redirect: redirectURL});
+function pageLoaded() {
+    var redirectURL = 'https://www.facebook.com/messages';
+    var needToMatch = /messages/;
+    var curURL = window.location.href;
+    if (curURL) {
+        if (!curURL.match(needToMatch)) {
+            // Sends to listener in redirect.js
+            chrome.extension.sendRequest({redirect: redirectURL});
+        }
     }
 }
 
-// Hide annoying elements (ID)... like the Facebook navigation bar
-var divsToHide = [
-    'pageHead',         // Header
-    'pagelet_ticker',   // Sidebar news
-    'pagelet_dock'      // Notifications that pop-up
-];
+function hideElements() {
+    var divsToHide = [
+        'pageHead',         // Header
+        'pagelet_ticker',   // Sidebar news
+        'pagelet_dock'      // Notifications that pop-up
+    ];
 
-divsToHide.forEach(function(elem, index, arr) {
-    var divToHide = elem;
-    var distractingDiv = document.getElementById(divToHide);
-    if (distractingDiv) {
-        distractingDiv.style.display = 'none';
-    }
-});
+    divsToHide.forEach(function(elem, index, arr) {
+        var divToHide = elem;
+        var distractingDiv = document.getElementById(divToHide);
+        if (distractingDiv) {
+            distractingDiv.style.display = 'none';
+            distractingDiv.style.height = 0;
+            distractingDiv.style.visibility = 'none';
+        }
+    });
+}
 
+// -------------------------------------------------- 
+// Setup on pageload
 
+// Catch links between Facebook pages
+var contentPane = document.getElementById('mainContainer');
+contentPane.addEventListener('DOMNodeRemovedFromDocument', 
+    pageLoaded, false);
+
+// Initial pageload
+pageLoaded();
+
+hideElements();
--- js/redirect.js
+++ js/redirect.js
@@ -1,4 +1,3 @@
 chrome.extension.onRequest.addListener(function(request, sender) {
-    console.log('request received');
     chrome.tabs.update(sender.tab.id, {url: request.redirect});
 });
