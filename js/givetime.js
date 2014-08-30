29e8eb7422436b56a087847288045262c1fe7fd6
Holly Xu <hjxu@uwaterloo.ca>
2014-08-30 09:47:35 -0700

    Popup now working as expected

--- js/givetime.js
+++ js/givetime.js
@@ -1,3 +1,10 @@
 // Give a certain amount of time
+var giveOne = document.getElementById('giveOne');
+var giveSix = document.getElementById('giveSix');
 
-console.log('hi');
+var giveTime = function(minutes) {
+	alert('Giving you ' + minutes + ' minutes');
+};
+
+giveOne.addEventListener('click', function() { giveTime(1); });
+giveSix.addEventListener('click', function() { giveTime(6); });
